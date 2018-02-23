f52542385f4c79caad640c1253b3cfb757dffd49
Tom Boutell <tom@punkave.com>
2018-02-23 08:40:54 -0500

    got the dashboard site to come up (have not tested it through the proxy yet)

--- README.md
+++ README.md
@@ -2,39 +2,60 @@
 
 ## A work in progress
 
+> This module requires Node 8. Your project must also have apostrophe as an npm dependency.
+
 ```javascript
 const multi = require('apostrophe-multisite')({
 
   // For speed, make sure a site is live on this many processes,
-  // on separate servers when possible. Setting this high
-  // uses more RAM
+  // on separate servers when possible. Processes are SHARED
+  // by MANY sites, but setting this high still uses more RAM
   concurrencyPerSite: 2,
+
   // If we receive no new requests for a site in an hour,
   // let that apos object go, freeing up RAM in exchange for
   // a little extra spinup time on the next request
   timeout: 60 * 60,
-  // We need to know what servers exist, if more than one
-  servers: [ 'frontend-1.example.com', 'frontend-2.example.com', 'frontend-3.example.com' ],
+
+  // We need to know what server/port combos are listening. This will
+  // match your setup in mechanic/nginx or other load balancer. Can
+  // also be space-separated in SERVERS env var
+
+  servers: [ 'localhost:3000', 'localhost:3001', 'localhost:3002' ],
   // ... And which server we are. Can also be set
-  // via `data/local.js` or APOS_MULTI_SERVER env var
-  server: 'frontend-1.example.com',
+  // via SERVER env var
+  
+  server: 'localhost:3000',
 
   // Apostrophe configuration for your hosted sites.
   // Just one config for all of them; per-site config could be
   // user editable settings in apostrophe-global
   sites: {
     modules: {
-      apostrophe-blog: { ... }
+      apostrophe-pages: {
+        choices: [
+          {
+            label: 'Home',
+            name: 'home'
+          },
+          {
+            label: 'Default',
+            name: 'default'
+          }
+        ]
+      }
     }
   },
 
   // Apostrophe configuration for the dashboard site.
+  // A `sites` module always exists, a piece that governs
+  // multisite management and has a hostnames property
   dashboard: {
     modules: {
-      // A pieces module that represents sites. Perhaps
-      // you add some custom fields in the usual way
-      'apostrophe-multisite-sites': {
-        addFields: [ ... plan name, billing stuff... ]
+      // Further configure the pieces module that represents sites. Perhaps
+      // you wish to add some custom fields in the usual way
+      'sites': {
+        addFields: [ ]
       }
     }
   }
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var _ = require('lodash');
+const _ = require('lodash');
+const mongo = require('mongodb');
+const fs = require('fs');
 
 module.exports = async function(options) {
 
@@ -29,12 +31,13 @@ module.exports = async function(options) {
     // Prefix, used primarily for database names. If you have
     // several multisite configurations going on (multiception!)
     // you'll need to make this unique for each collection of sites
-    shortNamePrefix: 'multisite-',
+    shortNamePrefix: process.env.SHORTNAME_PREFIX || 'multisite-',
     // MongoDB URL for database connection. If you have multiple physical
     // servers then you MUST configure this to a SHARED server (which
     // may be a replica set)
     mongodbUrl: process.env.MONGODB_URL || 'mongodb://localhost:27017',
-    dashboardHostname: process.env.DASHBOARD_HOSTNAME
+    dashboardHostname: process.env.DASHBOARD_HOSTNAME,
+    root: getRoot()
   });
   
   // All sites running under this process share a mongodb connection object
@@ -45,7 +48,8 @@ module.exports = async function(options) {
     reconnectInterval: 1000
   });
 
-  const apostrophe = require('apostrophe');
+  // required on behalf of the application, so it can see the peer dependency
+  const apostrophe = require(getNpmPath(options.root, 'apostrophe'));
   const httpProxy = require('http-proxy');
   const express = require('express');
   const app = express();
@@ -127,11 +131,11 @@ module.exports = async function(options) {
 
       if (req.headers['X-Apostrophe-Multisite-Spinup']) {
         if (!site.listeners[options.server]) {
-          site = await spinUpHere(site);
+          site = await spinUpHere(req, site);
         }
         winner = options.server;
       } else {
-        site = await spinUpAsNeeded(site);
+        site = await spinUpAsNeeded(req, site);
       }
 
       const keys = site.listeners.keys();
@@ -160,200 +164,257 @@ module.exports = async function(options) {
       console.error(e);
       return res.status(500).send('error');
     }
-
-    async function spinUpAsNeeded(site) {
-      while (site.listeners.keys().length < options.concurrencyPerSite) {
-        site = await spinUp(site);
-      }
-      return site;
-    }
-
-    async function spinUp(site) {
-      // Where should it be spun up?
-      // Preference for a separate physical server
-      const keys = site.listeners.keys();
-      let server = options.servers.find(server => {
-        return !keys.find(listener => {
-          return hostnameOnly(listener) === hostnameOnly(server);
-        });
-      });
-      if (!server) {
-        // Settle for a different core
-        server = options.servers.find(server => {
-          return !keys.find(listener => {
-            return listener === server;
-          });
-        });
-      }
-      if (!server) {
-        // It is already spun up everywhere
-        return site;
-      }
-      if (server === options.server) {
-        return spinUpHere(site);
-      } else {
-        return spinUpThere(site, server);
-      }
-    }
-
-    async function spinUpThere(site, server) {
-      req.headers['X-Apostrophe-Multisite-Spinup'] = 1;
-      return attempt(req, res, { target: 'http://' + server });
-    }
-
-    async function spinUpHere(site) {
-    
-      // The available free-port-finder modules all have race conditions and
-      // no provision for avoiding popular ports like 3000. Pick randomly and
-      // retry if necessary. -Tom
-  
-      const port = Math.floor(lowPort + Math.random() * totalPorts);
-
-      const apos = await require('util').promisify(run)(options.sites || {});
-
-      site.listeners[options.server] = hostnameOnly(options.server) = ':' + port;
-      const $set = {};
-      $set['listeners.' + options.server] = site.listeners[options.server];
-      await dashboard.docs.db.update({
-        _id: site._id
-      }, {
-        $set: set
-      });
-      
-      return site;
-      
-      function run(config, callback) {
-
-        let viewsFolderFallback = options.rootDir + '/sites/views';
-        if (!fs.existsSync(viewsFolderFallback)) {
-          viewsFolderFallback = undefined;
-        }
-
-        const apos = require('apostrophe')(
-
-          _.merge({
-
-            afterListen: function(err) {
-              if (err) {
-                // It's chill, try again until we get a free port.
-                return apos.destroy(function() {
-                  return run(config, callback);
-                });
-              }
-              apos._id = site._id;
-              return callback(null, apos);
-            },
-
-            modulesSubdir: options.rootDir + '/sites/lib/modules', 
-                 
-            shortName: options.shortNamePrefix + site._id,
-            
-            modules: {
-
-              'apostrophe-db': {
-                db: db
-              },
-            
-              'apostrophe-templates': {
-                viewsFolderFallback: viewsFolderFallback
-              },
-
-              'apostrophe-express': {
-                session: {
-                  secret: options.sessionSecret
-                },
-                forcePort: port
-              },
-              
-              'apostrophe-attachments': {
-                // TODO consider S3 in this context
-                uploadfs: {
-                  uploadsPath: __dirname + '/public/uploads/' + site._id,
-                  uploadsUrl: '/uploads/' + site._id,
-                  tempPath: __dirname + '/data/temp/' + site._id + '/uploadfs'
-                }
-              }
-            }
-          }, config)
-        );
-      }
-    }
-
-    async function spinUpDashboard() {
-
-      // TODO: this function has a lot of code in common with spinUpHere.
-      // Think about that. Should we support multiple constellations of
-      // sites in a single process, and just make the dashboard a specialized
-      // constellation at some point?
-
-      // The available free-port-finder modules all have race conditions and
-      // no provision for avoiding popular ports like 3000. Pick randomly and
-      // retry if necessary. -Tom
-  
-      const port = Math.floor(lowPort + Math.random() * totalPorts);
-
-      const apos = await require('util').promisify(run)(options.dashboard || {});
-      
-      return port;
-      
-      function run(config, callback) {
-
-        let viewsFolderFallback = options.rootDir + '/dashboard/views';
-        if (!fs.existsSync(viewsFolderFallback)) {
-          viewsFolderFallback = undefined;
-        }
-
-        const apos = require('apostrophe')(
-
-          _.merge({
-
-            afterListen: function(err) {
-              if (err) {
-                // It's chill, try again until we get a free port.
-                return apos.destroy(function() {
-                  return run(config, callback);
-                });
-              }
-              apos._id = site._id;
-              return callback(null, apos);
-            },
-
-            modulesSubdir: options.rootDir + '/dashboard/lib/modules', 
-                 
-            shortName: options.shortNamePrefix + 'dashboard',
-            
-            modules: {
-
-              'apostrophe-db': {
-                db: db
-              },
-            
-              'apostrophe-templates': {
-                viewsFolderFallback: viewsFolderFallback
-              },
-
-              'apostrophe-express': {
-                session: {
-                  secret: options.sessionSecret
-                },
-                forcePort: port
-              },
-              
-              'apostrophe-attachments': {
-                // TODO consider S3 in this context
-                uploadfs: {
-                  uploadsPath: __dirname + '/public/uploads/dashboard',
-                  uploadsUrl: '/uploads/dashboard',
-                  tempPath: __dirname + '/data/temp/dashboard/uploadfs'
-                }
-              }
-            }
-          }, config)
-        );
-      }      
-    }
-  
   }
+
+
+  async function spinUpAsNeeded(req, site) {
+    while (site.listeners.keys().length < options.concurrencyPerSite) {
+      site = await spinUp(req, site);
+    }
+    return site;
+  }
+
+  async function spinUp(req, site) {
+    // Where should it be spun up?
+    // Preference for a separate physical server
+    const keys = site.listeners.keys();
+    let server = options.servers.find(server => {
+      return !keys.find(listener => {
+        return hostnameOnly(listener) === hostnameOnly(server);
+      });
+    });
+    if (!server) {
+      // Settle for a different core
+      server = options.servers.find(server => {
+        return !keys.find(listener => {
+          return listener === server;
+        });
+      });
+    }
+    if (!server) {
+      // It is already spun up everywhere
+      return site;
+    }
+    if (server === options.server) {
+      return spinUpHere(site);
+    } else {
+      return spinUpThere(site, server);
+    }
+  }
+
+  async function spinUpThere(req, site, server) {
+    req.headers['X-Apostrophe-Multisite-Spinup'] = 1;
+    return attempt(req, res, { target: 'http://' + server });
+  }
+
+  async function spinUpHere(req, site) {
   
+    // The available free-port-finder modules all have race conditions and
+    // no provision for avoiding popular ports like 3000. Pick randomly and
+    // retry if necessary. -Tom
+
+    const port = Math.floor(lowPort + Math.random() * totalPorts);
+
+    const apos = await require('util').promisify(run)(options.sites || {});
+
+    site.listeners[options.server] = hostnameOnly(options.server) = ':' + port;
+    const $set = {};
+    $set['listeners.' + options.server] = site.listeners[options.server];
+    await dashboard.docs.db.update({
+      _id: site._id
+    }, {
+      $set: set
+    });
+    
+    return site;
+    
+    function run(config, callback) {
+
+      let viewsFolderFallback = getRootDir() + '/sites/views';
+      if (!fs.existsSync(viewsFolderFallback)) {
+        viewsFolderFallback = undefined;
+      }
+
+      const apos = apostrophe(
+
+        _.merge({
+
+          afterListen: function(err) {
+            if (err) {
+              // It's chill, try again until we get a free port.
+              return apos.destroy(function() {
+                return run(config, callback);
+              });
+            }
+            apos._id = site._id;
+            return callback(null, apos);
+          },
+
+          modulesSubdir: getRootDir() + '/sites/lib/modules', 
+               
+          shortName: options.shortNamePrefix + site._id,
+          
+          modules: {
+
+            'apostrophe-db': {
+              db: db
+            },
+          
+            'apostrophe-templates': {
+              viewsFolderFallback: viewsFolderFallback
+            },
+
+            'apostrophe-express': {
+              session: {
+                secret: options.sessionSecret
+              },
+              forcePort: port
+            },
+            
+            'apostrophe-attachments': {
+              // TODO consider S3 in this context
+              uploadfs: {
+                uploadsPath: __dirname + '/public/uploads/' + site._id,
+                uploadsUrl: '/uploads/' + site._id,
+                tempPath: __dirname + '/data/temp/' + site._id + '/uploadfs'
+              }
+            }
+          }
+        }, config)
+      );
+    }
+  }
+
+  async function spinUpDashboard() {
+
+    // TODO: this function has a lot of code in common with spinUpHere.
+    // Think about that. Should we support multiple constellations of
+    // sites in a single process, and just make the dashboard a specialized
+    // constellation at some point?
+
+    // The available free-port-finder modules all have race conditions and
+    // no provision for avoiding popular ports like 3000. Pick randomly and
+    // retry if necessary. -Tom
+
+    const port = Math.floor(lowPort + Math.random() * totalPorts);
+
+    const apos = await require('util').promisify(run)(options.dashboard || {});
+    
+    return port;
+    
+    function run(config, callback) {
+
+      let viewsFolderFallback = getRootDir() + '/dashboard/views';
+      if (!fs.existsSync(viewsFolderFallback)) {
+        viewsFolderFallback = undefined;
+      }
+
+      const apos = apostrophe(
+
+        _.merge({
+
+          afterListen: function(err) {
+            if (err) {
+              // It's chill, try again until we get a free port.
+              return apos.destroy(function() {
+                return run(config, callback);
+              });
+            }
+            apos._id = 'dashboard';
+            return callback(null, apos);
+          },
+
+          modulesSubdir: getRootDir() + '/dashboard/lib/modules', 
+                
+          shortName: options.shortNamePrefix + 'dashboard',
+          
+          modules: {
+
+            'apostrophe-db': {
+              db: db
+            },
+          
+            'apostrophe-templates': {
+              viewsFolderFallback: viewsFolderFallback
+            },
+
+            'apostrophe-express': {
+              session: {
+                secret: options.sessionSecret
+              },
+              forcePort: port
+            },
+            
+            'apostrophe-attachments': {
+              // TODO consider S3 in this context
+              uploadfs: {
+                uploadsPath: __dirname + '/public/uploads/dashboard',
+                uploadsUrl: '/uploads/dashboard',
+                tempPath: __dirname + '/data/temp/dashboard/uploadfs'
+              }
+            },
+
+            'sites-base': {
+              extend: 'apostrophe-pieces',
+              name: 'site',
+              addFields: [
+                {
+                  type: 'array',
+                  name: 'hostnamesArray',
+                  label: 'Hostnames',
+                  schema: [
+                    {
+                      type: 'string',
+                      required: true,
+                      name: 'hostname',
+                      help: 'All valid hostnames for the site must be on this list, for instance both example.com and www.example.com'
+                    }
+                  ]
+                }
+              ],
+              construct: function(self, options) {
+                self.beforeSave = function(req, doc, options, callback) {
+                  doc.hostnames = _.filter((doc.hostnamesArray && doc.hostnamesArray.value) || [], function(value) {
+                    return value.hostname.toLowerCase().trim();
+                  });
+                  return callback(null);
+                };
+              }
+            },
+
+            'sites': {
+              extend: 'sites-base'
+            }
+          }
+        }, config)
+      );
+    }      
+  }
+
+  function getRoot() {
+    let _module = module;
+    let m = _module;
+    while (m.parent) {
+      // The test file is the root as far as we are concerned,
+      // not mocha itself
+      if (m.parent.filename.match(/\/node_modules\/mocha\//)) {
+        return m;
+      }
+      m = m.parent;
+      _module = m;
+    }
+    return _module;
+  }
+
+  function getRootDir() {
+    var path = require('path');
+    return path.dirname(path.resolve(getRoot().filename));
+  }
+
+  function getNpmPath(root, type) {
+    const npmResolve = require('resolve');
+    return npmResolve.sync(type, { basedir: getRootDir() });
+  }
+
 };
 
