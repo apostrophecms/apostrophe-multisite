6d47e990f3a4d55b8fe907d9d9f185dd58da975a
Anthony Tarlao <anthony.tarlao@gmail.com>
2019-11-29 19:28:56 +0100

    update test

--- test/test.js
+++ test/test.js
@@ -1,133 +1,169 @@
-const mongo = require('mongodb')
 const del = require('del');
-const { expect } = require('chai')
-const rp = require('request-promise')
-const enableDestroy = require('server-destroy')
-const apostropheMultisite = require('../index.js')
+const mongo = require('mongodb');
+const { expect } = require('chai');
+const rp = require('request-promise');
+const enableDestroy = require('server-destroy');
+const apostropheMultisite = require('../index.js');
 
 describe('Apostrophe-multisite', function() {
   describe('#dashboard', function() {
+    const admin = 'admin';
+    const port = 3000;
+    const modules = {
+      'apostrophe-users': {
+        groups: [
+          {
+            title: 'admin',
+            permissions: ['admin']
+          }
+        ]
+      },
+      'apostrophe-pages': {},
+      'apostrophe-templates': {}
+    };
+    const title = 'New Site';
+    const url = 'site.test';
+    const shortNamePrefix = 'test-multi-';
 
-    let multisite
-    let sites
-    let site
-    let req
+    let multisite;
+    let sites;
+    let site;
+    let req;
 
     const newSite = {
-      title: 'New Site',
+      title,
       slug: 'new-site',
-      adminPassword: 'admin',
-      // devBaseUrl: 'site.test'
-    }
+      adminPassword: admin,
+      devBaseUrl: url,
+      hostnamesArray: [
+        {
+          hostname: url
+        }
+      ]
+    };
 
-    before(async() => {
+    before(async () => {
       //remove temp files
-      await del(['./test/sites/data'])
+      await del(['./test/sites/data']);
 
       // find and remove test dbs
-      const db = await mongo.MongoClient.connect('mongodb://localhost:27017')
-      const adminDb = db.admin()
-      const { databases } = await adminDb.listDatabases()
-      for(const db of databases) {
-        if (db.name.match(/^multisite-/)) {
-          const client = new mongo.Db(db.name, new mongo.Server('localhost', 27017))
-          await client.open()
-          await client.dropDatabase()
-          console.log('\x1b[36m%s\x1b[0m', `Test db ${db.name} dropped`)
+      const db = await mongo.MongoClient.connect('mongodb://localhost:27017');
+      const adminDb = db.admin();
+      const { databases } = await adminDb.listDatabases();
+      for (const db of databases) {
+        if (db.name.match(/^test-multi-/)) {
+          const client = new mongo.Db(db.name, new mongo.Server('localhost', 27017));
+          await client.open();
+          await client.dropDatabase();
+          console.log('\x1b[36m%s\x1b[0m', `Test db ${db.name} dropped`);
         }
       }
 
       // configure fake app using apostrophe-multisite
       multisite = await apostropheMultisite({
-         dashboardHostname: [
-           'dashboard.test'
-         ],
-         sessionSecret: 'test123',
-
-         sites: {
-           modules: {
-             'apostrophe-users': {
-               groups: [
-                 {
-                   title: 'admin',
-                   permissions: [ 'admin' ]
-                 }
-               ]
-             },
-             'apostrophe-pages': {},
-             'apostrophe-templates': {}
-           }
-         },
-
-         dashboard: {
-           modules: {
-             'apostrophe-users': {
-               groups: [
-                 {
-                   title: 'admin',
-                   permissions: [ 'admin' ]
-                 }
-               ]
-             },
-             'apostrophe-pages': {
-              park: [
-                 {
-                   title: 'Default',
-                   type: 'default',
-                   slug: '/default',
-                   published: true,
-                   orphan: true
-                 }
-               ],
-             },
-             'apostrophe-templates': {},
-           }
-         }
-       })
-       sites = multisite.apos.sites
-       site = sites.newInstance()
-       req = multisite.apos.tasks.getReq()
-    })
+        port,
+        sessionSecret: 'test123',
+        shortNamePrefix,
+        sites: { modules },
+        dashboard: { modules },
+        dashboardHostname: 'dashboard.test'
+      });
+      sites = multisite.apos.sites;
+      site = sites.newInstance();
+      req = multisite.apos.tasks.getReq();
+    });
 
     after(() => {
-      enableDestroy(multisite.server)
-      multisite.server.destroy()
-    })
+      enableDestroy(multisite.server);
+      multisite.server.destroy();
+    });
 
     it('starts the dashboard', async function() {
-      expect(multisite).to.be.an('object').that.has.any.keys('apos', 'server')
-    })
+      expect(multisite)
+        .to.be.an('object')
+        .that.has.any.keys('apos', 'server');
+    });
 
     it('creates a site', async function() {
-      expect(site).to.be.an('object')
-      expect(site.type).to.equal('site')
-    })
+      expect(site).to.be.an('object');
+      expect(site.type).to.equal('site');
+    });
 
     it('inserts a site and can find it', async function() {
-      const piece = await sites.insert(req, {...site, ...newSite})
-      expect(piece).to.be.an('object')
-      expect(piece).to.have.property('published', true)
-      expect(piece).to.have.property('title', 'New Site')
-      expect(piece).to.have.property('devBaseUrl', 'new-site.t')
-      expect(piece.slug).to.match(/^new-site/)
+      const piece = await sites.insert(req, { ...site, ...newSite });
+      expect(piece).to.be.an('object');
+      expect(piece).to.have.property('published', true);
+      expect(piece).to.have.property('title', title);
+      expect(piece).to.have.property('devBaseUrl', url);
+      expect(piece.slug).to.match(/^new-site/);
 
-      const found = await sites.find(req, { _id: piece._id}).toObject()
-      expect(found).to.be.an('object')
-      expect(found).to.have.property('published', true)
-      expect(found).to.have.property('title', 'New Site')
-      expect(found).to.have.property('devBaseUrl', 'new-site.t')
-      expect(found.slug).to.match(/^new-site/)
-    })
+      const found = await sites.find(req, { _id: piece._id }).toObject();
+      expect(found).to.be.an('object');
+      expect(found).to.have.property('published', true);
+      expect(found).to.have.property('title', title);
+      expect(found).to.have.property('devBaseUrl', url);
+      expect(found.slug).to.match(/^new-site/);
+    });
 
-    it.only('connects to the dashboard', async function() {
-      const dashboard = await rp('http://dashboard.test:3000')
-      expect(dashboard).to.have.string('<title>\n  \n    Home\n  \n</title>')
-      expect(dashboard).to.have.string(' <body class=" " data-apos-level="0">\n    \n    \n\n    \n      \n    \n    <div class="apos-refreshable" data-apos-refreshable>\n      \n  <header>\n    <nav class="o-container c-navigation">\n    \n    </nav>\n  </header>\n\n      <a name="main"></a>\n      \n      Home\n      \n      \n  <footer>\n\n  </footer>\n\n    </div>\n    <script>\nwindow.apos = {"prefix":"","csrfCookieName":"multisite-dashboard.csrf","uploadsUrl":"/uploads/dashboard"}\n</script>\n<script src="/modules/apostrophe-browser-utils/js/lean.js"></script>\n    \n      <script type="text/javascript">\n        \n        \n      </script>\n    \n    \n    \n  \n\n  </body>')
-    })
+    it('connects to the dashboard', async function() {
+      const dashboard = await rp(`http://dashboard.test:${port}`);
+      expect(dashboard).to.have.string('Home');
+      expect(dashboard).to.have.string('"csrfCookieName":"test-multi-dashboard.csrf"');
+    });
+
+    it('creates an admin user for the dashboard', async function() {
+      const adminGroup = await multisite.apos.groups.find(req, { title: admin }).toObject();
+      const adminUser = await multisite.apos.users.insert(req, {
+        username: admin,
+        password: admin,
+        title: admin,
+        groupIds: [adminGroup._id]
+      });
+      expect(adminUser).to.have.property('type', 'apostrophe-user');
+
+      try {
+        await rp({
+          method: 'POST',
+          uri: `http://dashboard.test:${port}/login`,
+          body: {
+            username: admin,
+            password: admin
+          },
+          json: true
+        });
+      } catch (error) {
+        expect(error).to.have.property('statusCode', 302);
+      }
+    });
 
     it('connects to the newly created site', async function() {
-      // const siteT = await rp('http://site.t:3000')
-      // console.log('siteT', require('util').inspect(siteT, { colors: true, depth: 1 }))
-    })
+      const piece = await sites.insert(req, {
+        ...site,
+        ...newSite,
+        devBaseUrl: 'site2.test',
+        hostnamesArray: [
+          {
+            hostname: 'site2.test'
+          }
+        ]
+      });
+      const siteT = await rp(`http://site2.test:${port}`);
+      expect(siteT).to.have.string('Home');
+      expect(siteT).to.have.string(`"csrfCookieName":"test-multi-${piece._id}.csrf"`);
+
+      try {
+        await rp({
+          method: 'POST',
+          uri: `http://site2.test:${port}/login`,
+          body: {
+            username: admin,
+            password: admin
+          },
+          json: true
+        });
+      } catch (error) {
+        expect(error).to.have.property('statusCode', 302);
+      }
+    });
   });
 });
